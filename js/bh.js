9f13e40fb62df30a033cb8d5284a3485c16b035d
Xavier Kyser <ac12b34u@gmail.com>
2025-04-28 20:44:44 -0400

    edits to bh html

--- js/bh.js
+++ js/bh.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
 function Info() {
   const info = document.getElementById("info");
   info.innerHTML = `
-    <h2>Crime Scene Cleaner Description</h2>
-    <p>A crime scene cleaner specializes in sanitizing and cleaning areas affected by crime or traumatic events, including those involving biohazards. They use specialized tools and chemicals to remove blood, bodily fluids, and other hazardous materials, ensuring the environment is thoroughly sanitized and safe.</p>
+    <h2>Bounty Hunter Description</h2>
+    <p>Bounty Hunters are private agents who capture fugitives or criminals for a commission or bounty, typically working for a bail bondsman. They are essentially the enforcers of the private bail system, hired to locate, apprehend, and return individuals who have skipped bail or failed to appear in court. </p>
     <img src="CSCimage/crimeSceneCleaner1.jpg" alt="CrimeSceneCleaner" class="img">
     `;
 }
@@ -18,23 +18,23 @@ function Salary() {
   const salary = document.getElementById("salary");
   salary.innerHTML = `
     <h2>Salary</h2>
-    <p>The national average salary for a crime scene cleaner is $8,293 per year</p>
+    <p>For a single bounty, the hunter usually profits about 10% - 25% amount of the posted bail bond amount. </p>
     `;
 }
 
 function Skill() {
   const skill = document.getElementById("skill");
   skill.innerHTML = `
-    <h2>Main Skill</h2>
+    <h2>Required Skills</h2>
     <ul>
-    <li>Cognizant: Identifies deficiencies and ensures removal of all hazardous waste.</li>
-    <li>Compassion: maintains empathy and professionalism when engaging with victims.</li>
+    <li>Invesitgative and Analytical Skills: The ability to locate, observe, and discern factors and pieces of information that help lead you to your bounty.</li>
+    <li>Legal Knowledge: Understand the limitations, and liberties of laws, in the county, state, and nation in which you would operate, so that you do not supersede your jurisdiction.</li>
     <li>Interpersonal Communication: communicates job details with clients and other professionals.</li>
-    <li>Biohazard Knowledge: Understand how to deal with blood borne pathogens.</li>
-    <li>Physical Strength: Facing long hours of intense work.</li>
-    <li>Safety Training: Ensure cleaning is done safely through certification.</li>
-    <li>Flexibility: Adjust work hours as needed.</li>
-    <li>Integrity: ensure confidentiality and respect on the job.</li>
+    <li>Resourcefulness and Perserverance: Using what is at your disposal at any given moment to be able to acquire your bounty, with the overall stamina in terms of completing the hunt.</li>
+    <li>Athleticism: Ability to operate in various conditions in altering severity rates.</li>
+    <li>Safety Training: Ensure the hunt is completed in the safest way for all parties involved.</li>
+    <li>Flexibility: Adjust hours of operation as needed, as well as managing fincnaces, and you are essentially your own employer.</li>
+    <li>Integrity: Ensuring the job is completed with morality, truthfulness, and justice intact.</li>
     </ul>
     `;
 }
@@ -42,7 +42,7 @@ function Skill() {
 function Game() {
   const training = document.getElementById("game");
   training.innerHTML = `
-    <h2>Crime Scene Cleaner Game</h2>
+    <h2>Bounty Hunter Game</h2>
     <p>"Once you get tangled with the mob, you will forever be a part of the mob. At least they pay a decent salary and all you have to do is clean up after their dirty work, but you know what you’re doing. So clean crime scenes, earn as much money as you can, and get ready for the next mission."</p>
     <ul>
     <li>Platforms: Xbox Cloud Gaming, GeForce Now, Microsoft Windows, Android</li>
@@ -62,19 +62,19 @@ function content(type) {
 
   if (type === "advantage") {
     content = `
-        <h3>Advantage</h3>
+        <h3>Advantages</h3>
         <ul>
-        <li>Stable Income</li>
-        <li>Job Fulfillment</li>
-        <li>Growing Demand in the Industry</li>
+        <li>Entreprenuership</li>
+        <li>Personal freedom in decisions over hours and jobs</li>
+        <li>Field work that nearly enforces accountability, especially with working out</li>
         </ul>
         `;
   } else if (type === "disadvantage") {
     content = `
-        <h3>Disadvantage</h3>
+        <h3>Disadvantages</h3>
         <ul>
-        <li>Facing bloody scenes</li>
-        <li>Psychologically stressful</li>
+        <li>Life or death danger risks</li>
+        <li>Monetary instability</li>
         </ul>
         `;
   }
